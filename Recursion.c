291b536f6201ec630bcd83a6fe5ca27457a1daed
parker <129785640+parkerstafford@users.noreply.github.com>
2023-09-16 17:41:41 -0800

    Create Recursion.c

---
+++ Recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void eatBanana(int bananas);
+
+int main() {
+
+    eatBanana(10);
+
+    return 0;
+}
+
+void eatBanana(int bananas) {
+    if (bananas < 1) return;
+    printf("You take ate a banana!\n");
+
+    eatBanana(bananas - 1);
+}
