9fb86b181a90761b6b8c3699b4bad706877e478b
parker <129785640+parkerstafford@users.noreply.github.com>
2023-12-25 13:09:18 -0700

    Update linearSearch.cs

--- linearSearch.cs
+++ linearSearch.cs
@@ -1,33 +1,26 @@
-using System;
+#include <stdio.h>
 
-class LinearSearch
-{
-    static int LinearSearch(int[] arr, int target)
-    {
-        for (int i = 0; i < arr.Length; i++)
-        {
-            if (arr[i] == target)
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    static void Main()
-    {
-        int[] arr = { 12, 45, 7, 23, 56, 89, 34, 8 };
-        int target = 23;
-
-        int result = LinearSearch(arr, target);
-
-        if (result != -1)
-        {
-            Console.WriteLine($"Element {target} found at index {result}.");
-        }
-        else
-        {
-            Console.WriteLine($"Element {target} not found in the array.");
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] == target) {
+            return i;
         }
     }
+    return -1; 
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50, 60};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 30;
+
+    int result = linearSearch(arr, n, target);
+
+    if (result != -1) {
+        printf("Element %d found at index %d.\n", target, result);
+    } else {
+        printf("Element %d not found in the array.\n", target);
+    }
+
+    return 0;
 }
